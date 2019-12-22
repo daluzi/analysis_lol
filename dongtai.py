a87a42f3023f9a841010e83a7651d027d1d146fb
daluzi <1210415250@qq.com>
2019-12-22 21:05:29 +0800

    dongtai

--- dongtai.py
+++ dongtai.py
@@ -40,6 +40,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+plt.rcParams['font.sans-serif']=['SimHei'] # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False # 用来正常显示负号
+
 overdoses = pd.read_excel('overdose_data_1999-2015.xls', sheet_name='Online', skiprows=6)
 def get_data(table, rownum, title):
 	data = pd.DataFrame(table.loc[rownum][2:]).astype(float)
@@ -48,7 +51,7 @@ def get_data(table, rownum, title):
 
 title = 'Heroin Overdoses'
 d = get_data(overdoses, 18, title)
-x = np.array(d.index)
+x = ['2013全球总决赛', '2013 LPL 春季赛', '2013 LPL 夏季赛', '2014 S4 全球总决赛', '2014 LPL 春季赛', '2014 LPL 夏季赛', '2015 LPL 春季赛', '2015 LPL 夏季赛', '2015全明星赛', '2015 德玛西亚杯—武汉', '2016 LPL 春季赛', '2016全球总决赛', '2016 LPL 夏季赛', '2016全明星赛', '2016德玛西亚杯—苏州武汉', '2017 LPL 春季赛', '2017 LPL 夏季赛']
 y = np.array(d['Heroin Overdoses'])
 overdose = pd.DataFrame(y, x)
 overdose.columns = {title}
@@ -57,17 +60,21 @@ overdose.columns = {title}
 # Writer = Writer(fps = 20, metadata = dict(artist = 'Me'), bitrate = 1800)
 
 fig = plt.figure(figsize=(10, 6))
-plt.xlim(1999, 2016)
+# plt.xlim(1999, 2016)
+plt.plot(x)
 plt.ylim(np.min(overdose)[0], np.max(overdose)[0])
 plt.xlabel('Year', fontsize = 20)
-plt.ylabel(title, fontsize = 20)
-plt.title('Heroin Overdoses per Year', fontsize = 20)
+plt.xticks(rotation=30)
+
+plt.ylabel('KDA', fontsize = 20)
+plt.title('UZI\'s KDA over the years', fontsize = 20)
 
 def animate(i):
 	data = overdose.iloc[:int(i + 1)]
 	p = sns.lineplot(x = data.index, y = data[title], data = data, color = 'r')
-	p.tick_params(labelsize = 17)
-	plt.setp(p.lines, linewidth = 7)
+	print(data[title])
+	p.tick_params(labelsize = 7)
+	plt.setp(p.lines, linewidth = 3)
 
 ani = matplotlib.animation.FuncAnimation(fig, animate, frames = 17, repeat=True)
 # ani.save('HeroinOverdosesJumpy.mp4', writer = Writer)
